9147cc4a9a5d699a8e89acdf6b6ab4da0456c67f
Suresh Alse <alseambusher@gmail.com>
2013-10-31 09:35:31 +0530

    subscribe on client done. todo server side

--- client/lib/protocol.py
+++ client/lib/protocol.py
@@ -3,24 +3,28 @@ import transport
 import user_layer
 from basic import get_response
 import threading
+from res import constants
 
 # step 1
 def send_API_key():
-    transport.data_request_with_response.append({'action':'REQUEST_GAMES','api_key':config.API_KEY})
-    data=get_response('action','RESPONSE_GAMES')
+    transport.data_request_with_response.append({'action':constants.REQUEST_GAMES,'api_key':config.API_KEY})
+    data=get_response('action',constants.RESPONSE_GAMES)
     select_game(data['games'])
 
 def select_game(games):
     session_key=user_layer.select_game(games)
     if session_key==-1:
-        transport.data_request_with_response.append({'action':'NEW_GAME','api_key':config.API_KEY})
+        transport.data_request_with_response.append({'action':constants.NEW_GAME,'api_key':config.API_KEY})
 
     else:
-        transport.data_request_with_response.append({'action':'SELECT_GAME','api_key':config.API_KEY,'session_key':session_key})
+        transport.data_request_with_response.append({'action':constants.SELECT_GAME,'api_key':config.API_KEY,'session_key':session_key})
 
+    # TODO if he cant connect to it u have to request again
     # should have 'session_key':'asdcasdc'
-    data=get_response('action','RESPONSE_SESSION_KEY')
+    data=get_response('action',constants.RESPONSE_SESSION_KEY)
     # now game is set
     config.SESSION_KEY=data['session_key']
 
-    #TODO subscribe to session key
+    # subscribe to session key
+    thread=threading.Thread(target=transport.server_subscribe, args=(config.SESSION_KEY,))
+    thread.start()
